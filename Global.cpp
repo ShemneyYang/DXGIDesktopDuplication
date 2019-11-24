8a7fdfd817db8added983ab34cd050ce69cadb4b
yangquanyu <yangquanyu@qq.com>
2019-11-24 21:19:34 +0800

    demo

---
+++ Global.cpp
@@ -0,0 +1,6 @@
+#include "Global.h"
+
+int Global::g_CaptureFps = 0;
+int Global::g_PaintFps = 0;
+int Global::g_PaintDelayTime = 0;
+int Global::g_LastFrameTime = 0;
---
+++ Global.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class Global
+{
+public:
+	static int g_CaptureFps;
+	static int g_PaintFps;
+	static int g_PaintDelayTime;
+	static int g_LastFrameTime;
+};
